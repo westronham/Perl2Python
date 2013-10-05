74deed489753a383b31aba764dab25e91b21627a
Ron <sacksackley@gmail.com>
2013-10-05 12:03:28 +1000

    Implementing Andrew's sample code
    Started implementing the Perl to Python modification code, beginning
    with working Andrew's sample code into the subroutine

--- perl2python.pl
+++ perl2python.pl
@@ -7,6 +7,9 @@ if (@ARGV == 1) {
    open (my $fh, "<", $ARGV[0]) or die "Can't open $ARGV[0]\n";
    my @file = <$fh>;
    my @python = conversion(@file);
+   foreach my $i (@python) {
+      print $i;
+   }
 } elsif (@ARGV == 0) {
    my @stdin = <>;
 } else {
@@ -17,6 +20,28 @@ if (@ARGV == 1) {
 sub conversion {
    my @convert = @_;
    my @python;
-   
+   foreach my $line (@convert) {
+      if ($line =~ /^#!/ && $. == 1) {
+	
+		   # translate #! line 
+		
+		   push(@python, "#!/usr/bin/python2.7 -u\n");
+	   } elsif ($line =~ /^\s*#/ || $line =~ /^\s*$/) {
+	
+		   # Blank & comment lines can be passed unchanged
+		
+		   push(@python, $line);
+	   } elsif ($line =~ /^\s*print\s*"(.*)\\n"[\s;]*$/) {
+		   # Python's print adds a new-line character by default
+		   # so we need to delete it from the Perl print statement
+		
+		   push(@python,"print \"$1\"\n");
+	   } else {
+	
+		   # Lines we can't translate are turned into comments
+		
+		   push(@python,"#$line\n");
+      }
+   }
    return @python;
 }
