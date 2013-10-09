58243d8638cd20504843abea0ae396d9643ccbd9
Ron <sacksackley@gmail.com>
2013-10-09 18:02:23 +1100

    Massive update
    Started from scratch, trying to make the code more organised and flow
    better

--- perl2python.pl
+++ perl2python.pl
@@ -20,53 +20,110 @@ if (@ARGV == 1) {
    die "Either 0 or 1 arguments";
 }
 
-# We have our array holding a Perl file and we want to turn it into a Python file
 sub conversion {
    my @convert = @_;
    my @python;
    foreach my $line (@convert) {
+      
+      # #! line
       if ($line =~ /^#!\//) {
-		   # translate #! line 
 		   push(@python, "#!/usr/bin/python2.7 -u\n");
 	   
+	   # Blank lines & Comments
 	   } elsif ($line =~ /^\s*#/ || $line =~ /^\s*$/) {
-		   # Blank & comment lines can be passed unchanged
 		   push(@python, $line);
-	   
-	   # We check if something is a variable which means it starts with my (optional) ($|%|@)[text]
-	   } elsif ($line =~ /^(my )?[@\$\%](.*) = (.*);$/) {
-	      my $varName = $2;
-	      my $value = $3;
+		   
+		# Variables
+	   } elsif ($line =~ /^\s*(my )?[\$](.*) = (.*);$/) {
 	      
-	      # Removes $ from variables in another variable 
-	      # 2 concerns: do we do this for arrays and hashes too and are we able to have $ signs in a variable string perhaps because this would delete those
-	      $value =~ tr/$//d;
+	      my $varName = $2;
+         my $value = $3;
+	      
+	      if ($value =~ /STDIN/i) {
+	         $value =~ s/$value/sys.stdin.readline()/
+	         
+	      } else {
+	         $value =~ tr/$//d;
+	      }
 	      
 	      push(@python, "$varName = $value\n");
-	   
-	   } elsif ($line =~ /^\s*print/x) {
+      
+      # Print
+      } elsif ($line =~ /^\s*print/) {
          
-         # If we're just printing a single variable
-	      if ($line =~ /\s*print\s*"\$([\w\d_]*)(\\n)?/i) {
-	         push(@python, "print $1\n");
-	     
-	     # When we want to print operations on variables 
-	     # Might need to modify to allow for more than 2 variables and 1 operation
-      } elsif (my @matches = $line =~ /\s*print\s*"?\$([\w\d_]*)\s*([\+\-\*\/\%])\s*\$([\w\d_]*).*/) {
-	      push(@python, "print $1 $2 $3\n");
-	     
-	     # Placeholder for when we want to print a string with variables
-	     
-	     # Printing plain string
-	      } elsif ($line =~ /^\s*print\s*['"](.*)\\n['"][\s;]*$/) {
-		      push(@python,"print \"$1\"\n");
-	      }
-	  
-	   } else {
+         # Remove semi-colon
+         $line =~ tr/;//d;
+         
+         # Remove \n
+         if ($line =~ /\n/) {
+            
+            # Remove lines that only print newline
+            if ($line =~ /^\s*print\s*"\\n"$/) {
+               next;
+            
+            # Remove newline that is appended after scalars
+            } elsif ($line =~ /, "\\n"/) {
+               $line =~ s/, "\\n"//g;
+            
+            # Remove newline from end of a string
+            } elsif ($line =~ /\\n"$/) {
+               $line =~ s/\\n"/"/g;
+            }
+         }
+         
+         # We don't need quotations if there is only one scalar
+         if ($line =~ /"\$([\w\d_]*)\s*"$/) {
+            $line =~ s/"//g;
+         }
+         
+         # Print with many variables
+         
+         # Remove $ from scalars
+         $line =~ tr/$//d;
+         
+         push(@python, "$line");
+      
+      # Loops   
+      } elsif ($line =~ /^\s*(if|while|for|foreach|elsif|else|unless)/) {
+         
+         # If/while statement
+         if ($line =~ /^\s*(if|while)/) {
+            $line =~ tr/$()//d;
+            $line =~ s/\s*{/:/;
+         }
+         
+         # Elsif/else statements
+         
+         # For loop
+         if ($line =~ /^\s*for/) {
+         
+         }
+         
+         # Foreach loop
+         
+         push(@python, $line);
+         
+         # Now we want to put the rest of the block into an array
+         my $indent_count = 1;
+         my @block;
+         while ($indent_count != 0) {
+            $line = shift @convert;
+            push(@block,$line);
+            if ($line =~ /{/) {
+               $indent_count += 1;
+            } elsif ($line =~ /}/) {
+               $indent_count -= 1;
+            }
+         }
+         foreach my $i (@block) {
+               print "BLOCK: $i";
+            }
+      
+      } else {
 	
 		   # Lines we can't translate are turned into comments
 		
-		   push(@python,"# $line\n");
+		   push(@python,"# $line");
       }
    }
    return @python;
